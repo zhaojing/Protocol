c119357276584c2c8152a54a2471f2b74f5de0ce
JingZhao <jingzhao@thoughtworks.com>
2017-01-12 10:36:25 +0800

    user model

---
+++ Protocol/model/User.swift
@@ -0,0 +1,29 @@
+//
+//  User.swift
+//  Protocol
+//
+//  Created by JingZhao on 1/12/17.
+//  Copyright © 2017 JingZhao. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    let name: String
+    let message: String
+    
+    init?(data: Data) {
+        guard let obj = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
+            return nil
+        }
+        guard let name = obj?["name"] as? String else {
+            return nil
+        }
+        guard let message = obj?["message"] as? String else {
+            return nil
+        }
+        
+        self.name = name
+        self.message = message
+    }
+}
